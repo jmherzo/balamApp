0af2d775893ab65733a7f5fb51f5db2b7b50cdc6
Jesus Manuel Hernandez Zozaya <jesus.herzo@outlook.com>
2018-08-05 14:28:23 -0500

    Added config restrictions

---
+++ src/constants/routes.js
@@ -0,0 +1,2 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
+// This file should be created personally
+import config from '../config/firebase';
 
 // Initialize Firebase for Balam App
-var config = {
-   apiKey: "AIzaSyDR3tX2BvjTwBM-UjddS-17EZxHY4KlzH0",
-   authDomain: "balam-app.firebaseapp.com",
-   databaseURL: "https://balam-app.firebaseio.com",
-   projectId: "balam-app",
-   storageBucket: "",
-   messagingSenderId: "13431631508"
-};
 firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
